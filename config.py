114e1c77c8bb8b5719970546ec66ff3db7649446
unknown <Aprendiz@BOGDFPCGMF4089.FormacionSena.RED>
2023-07-28 12:07:29 -0500

    proyecto creado

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    # Para conectar con la base de datos
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/flask_shopy_2687386'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
