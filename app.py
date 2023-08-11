921d3e34e90e359d596a8f08bf19079fb5683cfd
HernandezZuly <zulballes023@gmail.com>
2023-08-11 09:05:15 -0500

    Tabla

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Inicializar el objeto flask 
 app = Flask (__name__)
@@ -20,4 +21,14 @@ class Cliente(db.Model):
     password = db.Column(db.String(100))
     email = db.Column(db.String(100), unique = True)
     
-    
+class Producto(db.Model):
+    __tablename__ = "productos"
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(120), unique = True)
+    precio = db.Column(db.Numeric(precision = 10, scale = 2))
+    imagen = db.Column(db.String(120), unique = True)
+      
+class Venta(db.Model):
+    __tablename__ = "ventas"
+    id = db.Column(db.Integer, primary_key = True)
+    fecha = db.Column(db.DateTime, default = datetime.utcnow)
