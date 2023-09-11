a0d6eb22b3a1c2f6edd2348d0c8619bca4718b8b
HernandezZuly <zulballes023@gmail.com>
2023-09-11 08:33:41 -0500

    Add

--- app/clientes/forms.py
+++ app/clientes/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import InputRequired, Email
 
 class ClientForm():
     username = StringField("Ingrese el nombre de usuario: ",
                             validators = [ InputRequired(message = 'Usuario requerido') ])
     password = StringField("Ingrese la contraseña del cliente: ",
                             validators = [ InputRequired(message = 'Contraseña requerida') ])
-    email = StringField("Ingrese el correo del cliente: ",
-                            validators = [ InputRequired(message = 'Correo requerido') ])
+    email = EmailField("Ingrese el correo del cliente: ",
+                            validators = [ InputRequired(message = 'Correo requerido'), Email('El correo esta mal recuerde que se necesita un "@" y un "."')])
     
 class NewClientForm(FlaskForm, ClientForm):
     submit = SubmitField("Guardar")
